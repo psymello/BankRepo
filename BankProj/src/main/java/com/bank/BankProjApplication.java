a0097161d1e8d2105359a189525d7533194dda75
ADMIN <ADMIN@CONJUSTED>
2021-08-18 16:29:04 +0530

    first commit

---
+++ BankProj/src/main/java/com/bank/BankProjApplication.java
@@ -0,0 +1,13 @@
+package com.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankProjApplication.class, args);
+	}
+
+}
